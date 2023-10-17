98cc9669bc2c510fbe3be0d6fec11ec7a50f2b94
mrian11 <marian.yatsyk.ri.2023@lpnu.ua>
2023-10-17 13:26:36 +0300

    LAb_5.2

---
+++ Unit test Lab_5.2/Unit test Lab_5.2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_5.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnittestLab52
+{
+	TEST_CLASS(UnittestLab52)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double x = 0.1, n = 0, a = x, t1 = (2. * n - 1), t2 = (2. * n + 1);
+			double R = x * x * t1 / t2;
+			a *= R;
+		}
+	};
+}
